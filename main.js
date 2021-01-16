3bc9ca1ca1d2437ecece57032f86070c6745896c
robokiaan <71754527+robokiaan@users.noreply.github.com>
2021-01-16 15:22:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
-    document.getElementById("head").style.paddingLeft = "280px";
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("head").style.paddingLeft = "0";
 }
 
 jQuery(document).ready(function() {
     jQuery('.topic').hide()
     jQuery('#suggested').show()
 })
+
+function topic(name) {
+    jQuery('.topic').hide()
+    if (name == 'suggested') {
+        jQuery('#suggested').show()
+    } else if (name == 'fiction') {
+        jQuery('#fiction').show()
+    }
+}
