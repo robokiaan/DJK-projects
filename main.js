84b7d4b45f30b3429a68f72bc45c865b223dd4d6
robojai <77523831+robojai@users.noreply.github.com>
2021-01-16 18:00:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "280px";
     document.getElementById("main").style.marginLeft = "280px";
+    document.getElementById("head").style.paddingLeft = "280px";
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("head").style.paddingLeft = "0";
 }
 
 jQuery(document).ready(function() {
@@ -15,9 +17,13 @@ jQuery(document).ready(function() {
 
 function topic(name) {
     jQuery('.topic').hide()
-    if (name == 'suggested') {
+    if (name == "suggested") {
         jQuery('#suggested').show()
-    } else if (name == 'fiction') {
+    } else if (name == "fiction") {
         jQuery('#fiction').show()
     }
+    else if(name == "all") {
+        jQuery('#all').show()
+    }
+    closeNav()
 }
